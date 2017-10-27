01a070fe2778bc26725a7842bfa3e4f377c5cef1
xiaohuqi-j9 <xiaohuqi@126.com>
2017-10-27 13:38:43 +0800

    mongodb

---
+++ knowledge-map/src/main/java/com/hiekn/util/MongoDBConnectionUtil.java
@@ -0,0 +1,35 @@
+package com.hiekn.util;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+/**
+ * @author xiaohuqi@126.com
+ * @create 2017-06-19 22:32
+ **/
+public class MongoDBConnectionUtil {
+    private static MongoClient mongoClient = null;
+
+    public synchronized static MongoClient getMongoClient(){
+        if(mongoClient == null){
+            try {
+                MongoClientURI connectionString = new MongoClientURI("mongodb://centos-1:19130");
+                mongoClient = new MongoClient(connectionString);
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return mongoClient;
+    }
+
+    public static MongoDatabase getMongoDatabase(String dbName) {
+        return getMongoClient().getDatabase(dbName);
+    }
+
+    public static MongoCollection<Document> getMongoCollection(String dbName, String collectionName) {
+        return getMongoClient().getDatabase(dbName).getCollection(collectionName);
+    }
+}
