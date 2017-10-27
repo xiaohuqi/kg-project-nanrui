e1e0ee0f841b960c7e572641f18b4a1b9ed6278c
xiaohuqi-j9 <xiaohuqi@126.com>
2017-10-27 19:41:27 +0800

    dic

--- knowledge-map/src/main/java/com/hiekn/work/DicPreprocess.java
+++ knowledge-map/src/main/java/com/hiekn/work/DicPreprocess.java
@@ -1,5 +1,11 @@
 package com.hiekn.work;
 
+import com.hiekn.util.CommonResource;
+import com.hiekn.util.MongoDBConnectionUtil;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import org.bson.Document;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -14,8 +20,14 @@ import java.util.regex.Pattern;
 public class DicPreprocess {
 
 	public static void main(String[] args) {
-//		processDic("D:\\work\\guowang\\dic\\paper.dic");
-		compareDic("D:/work/guowang/dic/paper.dic", "D:/work/guowang/dic/process/paper.dic.dic");
+//		System.out.println("doubly-fed wind generators（dfwgs）".replaceAll("[\\(（][^\\(（\\)）]+?[\\)）]", ""));
+
+//		processDic(CommonResource.WORK_HOME.concat("dic/process/paper0.dic"));
+//		excludeStopword(CommonResource.WORK_HOME.concat("dic/process/paper0.dic.dic"), CommonResource.WORK_HOME.concat("dic/process/stopword.dic"));
+//		compareDic("D:/work/nanrui/dic/paper.dic", "D:/work/nanrui/dic/process/paper.dic.dic", "D:/work/nanrui/dic/process/stopword.dic");
+//		generateDicFromPaper(CommonResource.WORK_HOME.concat("dic/process/paper0.dic"));
+
+		findShortEnWords(CommonResource.WORK_HOME.concat("dic/paper.dic"), 2);
 	}
 
 
@@ -40,11 +52,15 @@ public class DicPreprocess {
 					continue;
 				}
 
-				line.replaceAll("[\\(（][^\\(（\\)）]+?[\\)）]", ""); //去除括号中内容
+				line = line.replaceAll("[\\(（][^\\(（\\)）]+?[\\)）]", "").trim(); //去除括号中内容
 
 				if(line.startsWith("“") && line.endsWith("”")){
 					line = line.substring(1);
-					line = line.substring(0, line.length() - 1);
+					line = line.substring(0, line.length() - 1).trim();
+				}
+				if(line.startsWith("\"") && line.endsWith("\"")){
+					line = line.substring(1);
+					line = line.substring(0, line.length() - 1).trim();
 				}
 
 				List<String> lineList = new ArrayList<String>();
@@ -94,14 +110,47 @@ public class DicPreprocess {
 		}
 	}
 
+	public static void excludeStopword(String dicPath, String sotpwordDicPath){
+		try {
 
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(sotpwordDicPath), "utf-8"));
+			String line = null;
+			Set<String> set = new HashSet<String>();
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				set.add(line);
+			}
+			br.close();
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dicPath.concat(".dic")), "utf-8"));
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(dicPath), "utf-8"));
+			while ((line = br.readLine()) != null) {
+				line = line.trim();
+				if(!line.equals("") && !set.contains(line)){
+					if(line.contains(" ") && line.getBytes("utf-8").length > line.length()){
+//						System.out.println(line);
+						continue;
+					}
+					bw.write(line.concat("\n"));
+				}
+				if(line.contains(" ") && line.matches("[\\s\\S]*[0-9]+[\\s\\S]*") && line.matches("[\\s\\S]*[a-z]+[\\s\\S]*")){
+					System.out.println(line);
+				}
+			}
+			br.close();
+			bw.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
 
 	/**
-	 * 比较词典，找出dic2中不在dic1中的词
+	 * 比较词典，找出dic2中不在dic1中的词，写入到dic3中
 	 * @param dicPath1	词典路径1
 	 * @param dicPath2	词典路径2
+	 * @param dicPath3	词典路径3
 	 */
-	public static void compareDic(String dicPath1, String dicPath2){
+	public static void compareDic(String dicPath1, String dicPath2, String dicPath3){
 		try {
 			Set<String> dicSet = new HashSet<String>();
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dicPath1), "utf-8"));
@@ -113,17 +162,88 @@ public class DicPreprocess {
 				}
 			}
 			br.close();
+			System.out.println(dicSet.size());
 
+			Set<String> set = new HashSet<String>();
 			br = new BufferedReader(new InputStreamReader(new FileInputStream(dicPath2), "utf-8"));
 			while((line = br.readLine()) != null){
 				line = line.trim();
 				if(!dicSet.contains(line)){
-					System.out.println(line);
+					set.add(line);
+//					System.out.println(line);
 				}
 			}
 			br.close();
-		}catch (Exception e){
 
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dicPath3), "utf-8"));
+			for(String word : set){
+				bw.write(word.concat("\n"));
+			}
+			bw.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void generateDicFromPaper(String dicPath){
+		try{
+			Set<String> set = new HashSet<String>();
+			MongoCollection mongoCollection = MongoDBConnectionUtil.getMongoCollection("source_data", "paper");
+			MongoCursor<Document> mongoCursor = mongoCollection.find().iterator();
+			while(mongoCursor.hasNext()){
+				Document document = mongoCursor.next();
+				if(document.containsKey("keywords")) {
+					List<String> keywords = (List<String>) document.get("keywords");
+					for(String word : keywords){
+						set.add(word.toLowerCase());
+					}
+//					System.out.println(keywords.size());
+				}
+				if(document.containsKey("authors")) {
+					List<Document> authors = (List<Document>) document.get("authors");
+					for(Document document1 : authors){
+						if(document1.containsKey("interests")){
+							List<String> keywords = (List<String>) document1.get("interests");
+							for(String word : keywords){
+								set.add(word.toLowerCase());
+							}
+						}
+					}
+
+				}
+			}
+			mongoCursor.close();
+			System.out.println(set.size());
+
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(dicPath), "utf-8"));
+			for(String word : set){
+				bw.write(word.concat("\n"));
+			}
+			bw.close();
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+	public static void findShortEnWords(String dicFilePath, int len){
+		try{
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(dicFilePath), "utf-8"));
+			String line = null;
+			Set<String> set = new HashSet<String>();
+
+			while((line = br.readLine()) != null){
+				if(set.contains(line)){
+					System.out.println(line);
+				}
+				set.add(line);
+//				if(line.length() == len && line.length() == line.getBytes("utf-8").length){
+//					System.out.println(line);
+//				}
+			}
+
+			br.close();
+		}catch(Exception e){
+			e.printStackTrace();
 		}
 	}
 }
